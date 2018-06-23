17abe04ef6ab90c04a961d3ccd365653295a13dc
John Fraser <jwfraser71@gmail.com>
2018-06-23 16:34:02 -0500

    Add Etch-A-Sketch project-finish

--- etchASketch.js
+++ etchASketch.js
@@ -21,3 +21,30 @@ resetGrid.addEventListener('click', (e) => {
         div.setAttribute('style', 'background: white');
     });
 });
+
+function makeGrid() {
+    clearBox();
+    gridLayout = window.prompt("How many grids do you want? (default: 16x16) ");
+
+    for (i = 0; i < gridLayout; i++) {
+        var row = '<div>';
+        for (j = 0; j < gridLayout; j++)
+            row += '<div class="square"></div>';
+            row += '</div>';
+            $grid.append(row);
+    }
+    var divGrid = document.querySelectorAll('.square');
+        divGrid.forEach((div) => {
+            div.addEventListener('mouseover', (e) => {
+            div.setAttribute('style', 'background: black');
+    });
+});
+}
+
+function clearBox() {
+    var div = document.querySelector('#pixelgrid');
+    while (div.firstChild){
+        div.removeChild(div.firstChild);
+
+    }
+}
